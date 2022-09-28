70ebc952998edbfdda1755001970ddbdeefe16b5
Latifah Zawedde <zawedde2021@gmail.com>
2022-09-27 17:20:13 -0700

    13commit

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3001
 app.use(express.json());
 app.use(express.urlencoded({ extend: true }));
 
+//app.use("/api", api);
+
 app.use(express.static("public"));
 
 app.get("/" , function(req, res) {
@@ -18,6 +20,10 @@ app.get("/notes" , function(req, res) {
 })
 
 app.use(routes);
+
+// app.get("*", (req, res) =>
+// 	res.sendFile(path.join(__dirname, "/public/index.html"))
+// );
 
 app.listen(PORT, () => console.log("Listing notes"))
 
